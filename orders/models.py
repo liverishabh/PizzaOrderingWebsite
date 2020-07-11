5057cfabd333f4220ba435aa27ebee40a4acf87f
liverishabh <rishabhranjan456@gmail.com>
2020-07-11 12:53:48 +0530

    Models added

--- orders/models.py
+++ orders/models.py
@@ -18,7 +18,7 @@ class Salads(models.Model):
 
 SIZES = (
     ('S', 'Small'),
-    ('L', 'Large'),
+    ('L', 'Large')
 )
 
 class DinnerPlatters(models.Model):
@@ -47,4 +47,28 @@ class Sub(models.Model):
             return f"{self.name}({self.get_size_display()}) - ${self.price} | No Extras"
         else:
             return f"{self.name}({self.get_size_display()}) - ${self.price} | Extras: {self.extras.in_bulk()}"
+
+class Toppings(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f"{self.name}"
+
+STYLES = (
+    ('R', 'Regular'),
+    ('S', 'Sicilian')
+)
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=32)
+    style = models.CharField(max_length=1, choices=STYLES)    
+    size = models.CharField(max_length=1, choices=SIZES)
+    price = models.DecimalField(max_digits=5, decimal_places=2, help_text='Price in US dollars')
+    toppings = models.ManyToManyField(Toppings, blank=True)
+
+    def __str__(self):
+        if self.toppings.count() == 0:
+            return f"{self.name}({self.get_style_display()} | {self.get_size_display()}) - ${self.price}"
+        else:
+            return f"{self.name}({self.get_style_display()} | {self.get_size_display()}) - ${self.price} | Toppings: {self.toppings.in_bulk()}"
 
