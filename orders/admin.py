a54cc19fa377fea1ee58e027f67ec9035113d9dc
liverishabh <rishabhranjan456@gmail.com>
2020-07-12 01:28:53 +0530

    Register models at admin site

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Pizza, Pasta, Sub, SubExtras, Salads, DinnerPlatters, Toppings
+
+admin.site.register(Pizza)
+admin.site.register(Pasta)
+admin.site.register(Sub)
+admin.site.register(SubExtras)
+admin.site.register(Salads)
+admin.site.register(DinnerPlatters)
+admin.site.register(Toppings)
