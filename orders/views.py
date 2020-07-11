6d7730962e3d89649d25a6209fc58de0f0fb3afe
liverishabh <rishabhranjan456@gmail.com>
2020-07-12 01:13:12 +0530

    Authentication System

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
 def index(request):
     return render(request, "orders/base.html")
