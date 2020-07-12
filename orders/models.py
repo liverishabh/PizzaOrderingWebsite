650b31e85cf25701476c58be43fd0b6dd473c76f
liverishabh <rishabhranjan456@gmail.com>
2020-07-12 22:12:38 +0530

    menu.html

--- orders/models.py
+++ orders/models.py
@@ -6,28 +6,29 @@ class Pasta(models.Model):
     name = models.CharField(max_length=32)
     price = models.DecimalField(max_digits=4, decimal_places=2, help_text='Price in US dollars')
 
-    def __str__(self):
-        return f"{self.name} - ${self.price}"
+    # def __str__(self):
+    #     return f"{self.name} - ${self.price}"
 
 class Salads(models.Model):
     name = models.CharField(max_length=32)
     price = models.DecimalField(max_digits=4, decimal_places=2, help_text='Price in US dollars')
 
-    def __str__(self):
-        return f"{self.name} - ${self.price}"
+    # def __str__(self):
+    #     return f"{self.name} - ${self.price}"
 
-SIZES = (
-    ('S', 'Small'),
-    ('L', 'Large')
-)
+# SIZES = (
+#     ('S', 'Small'),
+#     ('L', 'Large')
+# )
 
 class DinnerPlatters(models.Model):
     name = models.CharField(max_length=32)
-    size = models.CharField(max_length=1, choices=SIZES)
-    price = models.DecimalField(max_digits=5, decimal_places=2, help_text='Price in US dollars')
+    # size = models.CharField(max_length=1, choices=SIZES)
+    price_small = models.DecimalField(max_digits=5, decimal_places=2, help_text='Price in US dollars')
+    price_large = models.DecimalField(max_digits=5, decimal_places=2, help_text='Price in US dollars')
 
-    def __str__(self):
-        return f"{self.name}({self.get_size_display()}) - ${self.price}"
+    # def __str__(self):
+    #     return f"{self.name}({self.get_size_display()}) - ${self.price}"
 
 class SubExtras(models.Model):
     name = models.CharField(max_length=32)
@@ -38,15 +39,16 @@ class SubExtras(models.Model):
 
 class Sub(models.Model):
     name = models.CharField(max_length=32)
-    size = models.CharField(max_length=1, choices=SIZES)
-    price = models.DecimalField(max_digits=4, decimal_places=2, help_text='Price in US dollars')
+    # size = models.CharField(max_length=1, choices=SIZES)
+    price_small = models.DecimalField(max_digits=4, decimal_places=2, help_text='Price in US dollars')
+    price_large = models.DecimalField(max_digits=4, decimal_places=2, help_text='Price in US dollars')
     extras = models.ManyToManyField(SubExtras, blank=True)
 
-    def __str__(self):
-        if self.extras.count() == 0:
-            return f"{self.name}({self.get_size_display()}) - ${self.price} | No Extras"
-        else:
-            return f"{self.name}({self.get_size_display()}) - ${self.price} | Extras: {self.extras.in_bulk()}"
+    # def __str__(self):
+    #     if self.extras.count() == 0:
+    #         return f"{self.name}({self.get_size_display()}) | No Extras"
+    #     else:
+    #         return f"{self.name}({self.get_size_display()}) | Extras: {self.extras.in_bulk()}"
 
 class Toppings(models.Model):
     name = models.CharField(max_length=32)
@@ -61,14 +63,15 @@ STYLES = (
 
 class Pizza(models.Model):
     name = models.CharField(max_length=32)
-    style = models.CharField(max_length=1, choices=STYLES)    
-    size = models.CharField(max_length=1, choices=SIZES)
-    price = models.DecimalField(max_digits=5, decimal_places=2, help_text='Price in US dollars')
+    style = models.CharField(max_length=1, choices=STYLES)
+    # size = models.CharField(max_length=1, choices=SIZES)
+    price_small = models.DecimalField(max_digits=5, decimal_places=2, help_text='Price in US dollars')
+    price_large = models.DecimalField(max_digits=5, decimal_places=2, help_text='Price in US dollars')
     toppings = models.ManyToManyField(Toppings, blank=True)
 
-    def __str__(self):
-        if self.toppings.count() == 0:
-            return f"{self.name}({self.get_style_display()} | {self.get_size_display()}) - ${self.price}"
-        else:
-            return f"{self.name}({self.get_style_display()} | {self.get_size_display()}) - ${self.price} | Toppings: {self.toppings.in_bulk()}"
+    # def __str__(self):
+    #     if self.toppings.count() == 0:
+    #         return f"{self.name}({self.get_style_display()} | {self.get_size_display()}) - ${self.price}"
+    #     else:
+    #         return f"{self.name}({self.get_style_display()} | {self.get_size_display()}) - ${self.price} | Toppings: {self.toppings.in_bulk()}"
 
--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .models import Pizza, Pasta, Sub, SubExtras, Salads, DinnerPlatters, Toppings
+
 @login_required
 def index(request):
-    return render(request, "orders/base.html")
+    context ={
+        "pizzas" : Pizza.objects.all(),
+        "toppings" : Toppings.objects.all(),
+        "subs" : Sub.objects.all(),
+        "subextras" : SubExtras.objects.all(),
+        "pastas" : Pasta.objects.all(),
+        "dinnerplatters" : DinnerPlatters.objects.all(),
+        "salads" : Salads.objects.all(),
+    }
+    return render(request, "orders/menu.html", context)
